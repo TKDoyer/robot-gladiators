12f402beeba46093fdc611b62631ce3ee86fb9eb
Robert Doyer <tkdoyer@gmail.com>
2022-05-20 20:24:54 -0500

    robot gladiators code start

---
+++ assets/game.js
@@ -0,0 +1,11 @@
+// Game States
+
+// "WIN" - Player robot has defeated all enemy-robots
+
+// * Fight all enemy-robots
+
+// * Defeat each enemy-robots
+
+// "LOSE" - Player robot's health is zero or less
+
+window.alert("This is an alert! JavaScript is Running!");
